691eb473cb8e655907055968bf4dff5f7ef9a30b
OtnielSSilva <otniel.dss04@gmail.com>
2024-05-31 17:47:31 -0400

    feat: initiates project

---
+++ backend/src/routes/user.ts
@@ -0,0 +1,71 @@
+import { Router } from 'express';
+import prisma from '../prisma';
+
+const router = Router();
+
+// Get all users
+router.get('/', async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch users' });
+  }
+});
+
+// Get a user by ID
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await prisma.user.findUnique({ where: { id: String(id) } });
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch user' });
+  }
+});
+
+// Create a new user
+router.post('/', async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const newUser = await prisma.user.create({
+      data: { name, email, password },
+    });
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to create user' });
+  }
+});
+
+// Update a user by ID
+router.post('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { name, email, password } = req.body;
+  try {
+    const updateUser = await prisma.user.update({
+      where: { id: String(id) },
+      data: { name, email, password },
+    });
+    res.json(updateUser);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to update user' });
+  }
+});
+
+// Delete a user by ID
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    await prisma.user.delete({
+      where: { id: String(id) },
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to delete user' });
+  }
+});
+
+export default router;
